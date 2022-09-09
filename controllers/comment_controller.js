b7f5017b0548625bc4859f2e9992ba8013d3f33d
Ayush <aayushgupta023@gmail.com>
2022-09-09 12:12:56 +0530

    upd

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,35 +1,39 @@
-const commentModel = require('../models/comments');
-const postModel = require('../models/post');
-module.exports.create = function(req,res){
-    //check first if the post is present or not
-    postModel.findById(req.body.post,function(err,post){
-        if(post){
-            commentModel.create(req.body,function(err,comment){
-                try{
-                    post.comments.push(comment._id);
-                    //save() function is used when u make some changes into the db's data and then u are required to save it 
-                    //otherwise this would have been achieved by-> first remove the data from db.. update it and then recreate it into db
-                    post.save();
-                return res.redirect('/users/profile');
-                }catch(err){
-                    return res.redirect('back');
-                }
-            });
-        }else{
-            return res.redirect('back');
-        }   
-    });
-}
-module.exports.destroy = function(req,res){
-    commentModel.findById(req.params.id, function(err,comment){
-        if(comment.user == req.user.id){
-            let postid = comment.post;
-            comment.remove();
-            postModel.find(postid, {$pull:{comments:req.params.id}},function(err, post){
-                return res.redirect('back');
-            })
-        }else{
-            return res.redirect('back');
+const commentModel = require("../models/comments");
+const postModel = require("../models/post");
+module.exports.create = function (req, res) {
+  //check first if the post is present or not
+  postModel.findById(req.body.post, function (err, post) {
+    if (post) {
+      commentModel.create(req.body, function (err, comment) {
+        try {
+          post.comments.push(comment._id);
+          //save() function is used when u make some changes into the db's data and then u are required to save it
+          //otherwise this would have been achieved by-> first remove the data from db.. update it and then recreate it into db
+          post.save();
+          return res.redirect("/users/profile");
+        } catch (err) {
+          return res.redirect("back");
         }
-    })
-}
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+module.exports.destroy = function (req, res) {
+  commentModel.findById(req.params.id, function (err, comment) {
+    if (comment.user == req.user.id) {
+      let postid = comment.post;
+      comment.remove();
+      postModel.find(
+        postid,
+        { $pull: { comments: req.params.id } },
+        function (err, post) {
+          return res.redirect("back");
+        }
+      );
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
